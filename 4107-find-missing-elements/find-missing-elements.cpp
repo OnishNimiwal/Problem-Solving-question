9f4e5911f984acf696ffa18df9d003a8f0c72b8c
Onish Nimiwal <151050717+OnishNimiwal@users.noreply.github.com>
2026-08-04 19:50:25 +0530

    Time: 11 ms (6.06%) | Memory: 33.8 MB (8.08%) - LeetSync

---
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>mpp;
+        int mini=INT_MAX;
+        int maxi=INT_MIN;
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            mpp[nums[i]]++;
+            mini=min(mini,nums[i]);
+            maxi=max(maxi,nums[i]);
+        }
+        for(int i=mini;i<=maxi;i++){
+            if(mpp.find(i)==mpp.end())ans.push_back(i);
+        }
+        return ans;
+    }
+};
